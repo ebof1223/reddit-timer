9b0c7bc8d078eb4599ecc3d984e2735bbabb6c20
Erwin Bofetiado <erwinjbofetiado@gmail.com>
2021-09-10 19:46:00 -0500

    added normalize, global styles, header to app

--- src/AppStyle.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const AppStyle = styled.div`
-  font-family: Montserrat;
-  color: #93918f;
-`;
-
-export default AppStyle;
---
+++ src/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: '#93918f';
+    font-family: Montserrat;
+  }
+`;
+
+export default GlobalStyle;
