5dddf4a2a40ca92175d0e854079f580671b067ee
Erwin Bofetiado <erwinjbofetiado@gmail.com>
2021-09-13 16:50:48 -0500

    rendered full heatmap with unique keys

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -2,62 +2,28 @@ import React from 'react';
 
 import { Hero, Time, Group, Element, Day } from './table.styles';
 import generate24HrPostTimes from './table-times-generate';
+import { weekDays } from './weekDays';
 
 const Table = () => {
+  const times = [null, ...generate24HrPostTimes()];
+
   return (
     <Hero>
-      {[null, ...generate24HrPostTimes()].map((time) => (
+      {times.map((time) => (
         <Time key={time}>{time}</Time>
       ))}
-      <Day>Monday</Day>
-      <Group>
-        <Element>1</Element>
-        <Element>1</Element>
-      </Group>
-      <Group>
-        <Element>1</Element>
-        <Element>1</Element>
-      </Group>
-      <Group>
-        <Element>1</Element>
-        <Element>1</Element>
-      </Group>
-      <Group>
-        <Element>1</Element>
-        <Element>1</Element>
-      </Group>
-      <Group>
-        <Element>1</Element>
-        <Element>1</Element>
-      </Group>
-      <Group>
-        <Element>1</Element>
-        <Element>1</Element>
-      </Group>
-      <Group>
-        <Element>1</Element>
-        <Element>1</Element>
-      </Group>
-      <Group>
-        <Element>1</Element>
-        <Element>1</Element>
-      </Group>
-      <Group>
-        <Element>1</Element>
-        <Element>1</Element>
-      </Group>
-      <Group>
-        <Element>1</Element>
-        <Element>1</Element>
-      </Group>
-      <Group>
-        <Element>1</Element>
-        <Element>1</Element>
-      </Group>
-      <Group>
-        <Element>1</Element>
-        <Element>1</Element>
-      </Group>
+
+      {weekDays.map((day, index) => (
+        <React.Fragment key={day}>
+          <Day key={`${day}-${index}`}>{day}</Day>
+          {times.slice(1).map((time, index) => (
+            <Group key={`${day}-${time}-group-${index}`}>
+              <Element key={`${day}-${time}-item-0`}>1</Element>
+              <Element key={`${day}-${time}-item-1`}>1</Element>
+            </Group>
+          ))}
+        </React.Fragment>
+      ))}
     </Hero>
   );
 };
---
+++ src/components/Table/weekDays.js
@@ -0,0 +1,9 @@
+export const weekDays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
